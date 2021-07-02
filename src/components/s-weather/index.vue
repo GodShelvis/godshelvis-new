f4eb6b0989d9ef7af70a3440c686659222f23af6
Shelvis <45034892+GodShelvis@users.noreply.github.com>
2021-07-02 17:22:11 +0800

    2020_07_02

--- src/components/s-weather/index.vue
+++ src/components/s-weather/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="weather-area no-select">
-    <img v-if="loadOver" class="weather-icon" :src="`/static/s2/${weather.icon}.png`" alt="">
-    <div v-if="loadOver" class="weather-info">
-      <p class="weather-location">{{location.adm1}}/{{location.name}}</p>
-      <p class="weather-message">{{weather.temp}}°C {{weather.text}}</p>
+  <div class="no-select weather-area" @mouseenter="enterHover" @mouseleave="leaveHover">
+    <div v-if="loadOver">
+      <div v-if="s1show" class="weather">
+        <img class="weather-icon s1" :src="`/static/s1/${weather.icon}.png`" alt="">
+        <div class="weather-info">
+          <p class="weather-location">{{location.adm1}}/{{location.name}}</p>
+          <p class="weather-message">{{weather.temp}}°C {{weather.text}}</p>
+        </div>
+      </div>
     </div>
+<!--    <transition>-->
+<!--      <div v-if="loadOver && s2show" class="weather-active">-->
+<!--        <img class="weather-icon" :src="`/static/s2/${weather.icon}.png`" alt="">-->
+<!--        <div class="weather-info">-->
+<!--          <p class="weather-location">{{location.adm1}}/{{location.name}}</p>-->
+<!--          <p class="weather-message">{{weather.temp}}°C {{weather.text}}</p>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </transition>-->
   </div>
 </template>
 
@@ -19,13 +32,16 @@ let weather = ref({});
 let locationApi:string
 let weatherApi:string
 let loadOver = ref(false)
+let hoverStatus = ref(false)
+let s1show = ref(true)
+let s2show = ref(false)
 
 navigator.geolocation.getCurrentPosition((positon)=>{
   longitude.value = Number(positon.coords.longitude.toFixed(2))
   latitude.value = Number(positon.coords.latitude.toFixed(2))
   locationApi = `https://geoapi.qweather.com/v2/city/lookup?location=${longitude.value},${latitude.value}&key=6099ae81fe7a4d508f625c01cebe41a3`
   weatherApi = `https://devapi.qweather.com/v7/weather/now?location=${longitude.value},${latitude.value}&key=6099ae81fe7a4d508f625c01cebe41a3`
-  
+
   fly.get(locationApi).then((res)=>{
     if (res.data.code == 200){
       location.value = res.data.location[0]
@@ -46,6 +62,22 @@ navigator.geolocation.getCurrentPosition((positon)=>{
   console.log(err);
 })
 
+const enterHover = function () {
+  hoverStatus.value = false
+  s1show.value = false
+  setTimeout(()=>{
+    s2show.value = true
+  },500)
+}
+
+const leaveHover = function () {
+  hoverStatus.value = true
+  s2show.value = false
+  setTimeout(()=>{
+    s1show.value = true
+  },500)
+}
+
 </script>
 
 <style scoped>
@@ -57,13 +89,10 @@ navigator.geolocation.getCurrentPosition((positon)=>{
   border: 0px #f2f2f2 solid;
   box-sizing: border-box;
   box-shadow: -1px -1px 1px #FFFFFF, 1px 1px 1px #BEBEBE, 0px 0px 0px #97C8EB inset, 0px 0px 0px #1D6A9F inset;
-  transition: 0.2s;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  transition: 2s;
 }
 .weather-area:hover{
+  color: #485156;
   background-color: #CDE7F6;
   border: 5px #F2F2F2 solid;
   box-shadow: -10px -10px 30px #FFFFFF, 10px 10px 30px #BEBEBE, -10px -10px 30px #97C8EB inset, 10px 10px 30px #1D6A9F inset;
@@ -73,12 +102,8 @@ navigator.geolocation.getCurrentPosition((positon)=>{
   width: 160px;
   /* background-color: #fff; */
 }
-.s1{
-  margin: 0 20px;
-  width: 120px;
-}
 .weather-info{
-  width: 120px;
+  width: 160px;
   margin-right: 20px;
   display: flex;
   flex-direction: column;
@@ -86,11 +111,35 @@ navigator.geolocation.getCurrentPosition((positon)=>{
   align-items: flex-end;
 }
 .weather-location{
-  margin: 0 0 10px 0;
-  font-size: 13px;
+  margin: 0 0 5px 0;
+  font-size: 15px;
 }
 .weather-message{
   margin: 0;
-  font-size: 25px;
+  font-size: 35px;
+}
+
+.weather{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.weather-active{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+s1-enter-from,
+s1-leave-to{
+ opacity: 0;
+}
+s1-leave-from,
+s1-enter-to{
+  opacity: 1;
+}
+s1-enter-active{
+  transition: all 0.1s ease-in;
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,90 @@
 <template>
-  <div>home</div>
+  <div class="home-layor">
+    <div class="home-list">
+      <SSearch></SSearch>
+      <SBookmark style="margin-top: 30px" v-model:bookmark="bookmarks1"></SBookmark>
+      <SBookmark style="margin-top: 30px" v-model:bookmark="bookmarks2"></SBookmark>
+    </div>
+    <div class="plugin-list">
+      <SCalendar style="margin-bottom: 30px"></SCalendar>
+      <div style="width: 100%; display: flex; flex-direction: row; justify-content: space-between;">
+        <SWeather></SWeather>
+        <SClock></SClock>
+      </div>
+      <STranslate style="margin-top: 30px"></STranslate>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import SSearch from '../../components/s-search'
+import SBookmark from '../../components/s-bookmark'
+import SCalendar from '../../components/s-calendar'
+import SWeather from '../../components/s-weather'
+import SClock from '../../components/s-clock'
+import STranslate from '../../components/s-translate'
+
+const bookmarks1 = {
+  title: '视频',
+  color: '#F26D48',
+  bookmarks: [
+    {
+      name: '音范丝',
+      description: '音范丝|影音集|精选4K蓝光原盘下载，顶级收藏',
+      url: 'www.yinfans.me'
+    },
+    {
+      name: '美剧迷',
+      description: '美剧大全,最新好看的天天美剧下载,在线观看,美剧天堂',
+      url: 'www.yinfans.me'
+    }
+  ]
+}
+const bookmarks2 = {
+  title: '视频',
+  color: '#75CD5F',
+  bookmarks: [
+    {
+      name: '淘宝',
+      description: '淘宝网',
+      url: 'www.yinfans.me'
+    },
+    {
+      name: 'MacWk',
+      description: 'MacWk - 精品mac软件下载',
+      url: 'www.yinfans.me'
+    },
+    {
+      name: '扫雷',
+      description: 'Minesweeper Online',
+      url: 'www.yinfans.me'
+    }
+  ]
+}
 </script>
 
 <style scoped>
-
+.home-layor {
+  width: 1440px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.home-list{
+  margin: 50px 75px;
+  width: 600px;
+  min-height: 70vh;
+  /*background-color: #4f3535;*/
+  display: flex;
+  flex-direction: column;
+}
+.plugin-list{
+  margin: 50px 75px;
+  width: 540px;
+  min-height: 70vh;
+  /*background-color: #4f3535;*/
+  display: flex;
+  flex-direction: column;
+}
 </style>
